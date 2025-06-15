e1dc85d255f0f22b6054b7f6f2181b5dde8b917b
01sapankumar <ksapan73@gmail.com>
2025-06-15 23:24:39 +0530

    Updated API URL in AppState.jsx

--- src/context/AppState.jsx
+++ src/context/AppState.jsx
@@ -6,7 +6,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const AppState = (props) => {
 
-  const url = "http://localhost:1000/api";
+  //const url = "http://localhost:1000/api";
+  const url = "https://mern-e-commerce-9opy.onrender.com/api";
   const [token, setToken] = useState();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [filteredData, setFilteredData] = useState([]);
